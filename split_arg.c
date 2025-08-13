c8d18b282180dc1e002c5291c64ac94c3803a52b
Mathieu <mathieu.mjr@gmail.com>
2025-08-13 10:35:54 +0200

    modified split arg so it can take different delimiter

--- split_arg.c
+++ split_arg.c
@@ -6,23 +6,24 @@
  *
  * Return: An array like argv
  */
-char **split_string(char *str)
+char **split_string(char *str, const char *sep)
 {
 	int i = 0, j = 0, word_num = 1;
 	char *token;
 	char **arg;
+	char charsep = sep[0];
 
 	if (str == NULL)
 		return (NULL);
 
-	if (str[0] == ' ')
+	if (str[0] == charsep)
 		word_num = 0;
 
 	while (str[i])
 	{
 		if (str[i + 1] != '\0')
 		{
-			if (str[i] == ' ' && str[i + 1] != ' ')
+			if (str[i] == charsep && str[i + 1] != charsep)
 				word_num++;
 		}
 		i++;
@@ -33,12 +34,12 @@ char **split_string(char *str)
 		return (NULL);
 
 	arg[word_num] = NULL;
-	token = strtok(str, " ");
+	token = strtok(str, sep);
 	while (token != NULL)
 	{
 		arg[j] = token;
 		j++;
-		token = strtok(NULL, " ");
+		token = strtok(NULL, sep);
 	}
 
 	return (arg);
