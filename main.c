aa6ca324d71f1f829f7d0fcf6bb1c0482a730c72
Mathieu <mathieu.mjr@gmail.com>
2025-08-13 11:37:59 +0200

    tested  getpath in main

--- main.c
+++ main.c
@@ -14,48 +14,49 @@ int main(void)
 	char **argv;
 	pid_t fpid;
 
+
 	/*commands_t build_in[] = {
 		{"c", print_char},
 		{"s", print_string},
 		{NULL, NULL},
 	};*/
-
+	get_paths(environ);
 	while (1)
 	{
-	if (isatty(STDIN_FILENO) == 1)
-		printf("prompt$ ");
-	nread = getline(&line, &len, stdin);
-	/*Don't forget to free getline*/
-	if (nread == -1)
-		break;
-	if (nread == 1)
-		continue;
-	if ((nread > 0) && (line[nread - 1] == '\n'))
-		line[nread - 1] = '\0';
+		if (isatty(STDIN_FILENO) == 1)
+			printf("prompt$ ");
+		nread = getline(&line, &len, stdin);
+		/*Don't forget to free getline*/
+		if (nread == -1)
+			break;
+		if (nread == 1)
+			continue;
+		if ((nread > 0) && (line[nread - 1] == '\n'))
+			line[nread - 1] = '\0';
 	
-	argv = split_string(line, " ");
-	if (argv[0] == NULL)
-	{
-		free(argv);
-		continue;
-	}
-
-	fpid = fork();
-	if (fpid == -1)
-	{
-		perror("fpid");
-		exit(EXIT_FAILURE); /*préciser*/
-	}
-	else if (fpid == 0) /*enfant*/
-	{
-		if (execve(argv[0], argv, environ) == -1)
+		argv = split_string(line, " ");
+		if (argv[0] == NULL)
 		{
-			perror("./prompt");
+			free(argv);
+			continue;
+		}
+
+		fpid = fork();
+		if (fpid == -1)
+		{
+			perror("fpid");
 			exit(EXIT_FAILURE); /*préciser*/
 		}
-	}
-	else
-		waitpid(fpid, NULL, 0);
+		else if (fpid == 0) /*enfant*/
+		{
+			if (execve(argv[0], argv, environ) == -1)
+			{
+				perror("./prompt");
+				exit(EXIT_FAILURE); /*préciser*/
+			}
+		}
+		else
+			waitpid(fpid, NULL, 0);
 	}
 
 	return (0);
